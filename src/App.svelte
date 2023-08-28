fd89659f39d90a7f7cdb27391923aee39cfd0bb4
rash <orashusedmund@gmail.com>
2023-08-28 06:44:32 +0100

    intial setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
   import TailwindCss from "./TailwindCSS.svelte";
-  import svelteLogo from "./assets/svelte.svg";
-  import viteLogo from "/vite.svg";
   import Counter from "./components/Counter.svelte";
-  import Todo from "./routes/+page.svelte";
 </script>
 
 <TailwindCss />
 <main class="min-w-[400px] max-w-[600px] bg-slate-800">
   <Counter />
-  <Todo />
 </main>
